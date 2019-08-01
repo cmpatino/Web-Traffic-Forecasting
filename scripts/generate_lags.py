3e9075a48822a7960dad8330adf5018bd7196bc0
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2019-08-01 11:35:49 -0500

    remove unnecessary parameters

--- scripts/generate_lags.py
+++ scripts/generate_lags.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 DATA_PATH = '../data/input/'
 
@@ -18,6 +19,7 @@ def get_lag_optimized(timeseries, lag_values, n_days):
 
     n_features = len(lag_values) + 1
     features = np.empty((n_days, n_features))
+    median = np.nanmedian(timeseries)
     for i in range(timeseries.shape[0]):
         x = np.zeros((n_features))
         x[0] = timeseries[i]
@@ -26,38 +28,42 @@ def get_lag_optimized(timeseries, lag_values, n_days):
             lag_index = i - lag
 
             if lag_index < 0:
-                x[j + 1] = 0
+                x[j + 1] = median
             else:
                 x[j + 1] = timeseries[lag_index]
+
         features[i] = x
 
     return features
 
 
-def create_features_optimized(DATA_PATH, n_series, n_days, n_features):
+def create_features_optimized(DATA_PATH):
     """Create lagging features for all timeseries
 
     Arguments:
         DATA_PATH {str} -- path to data location
-        n_series {int} -- number of timeseries in data
-        n_days {int} -- number of days in timeseries
-        n_features {int} -- number of total features
 
     Returns:
         np.ndarray -- Array with shape (n_series, n_days, n_features)
     """
 
     train_df = pd.read_csv(DATA_PATH + 'train_2.csv')
+    n_series = train_df.shape[0]
+    n_days = train_df.shape[1] - 1
+    lag_values = [365, 180, 90]
+    n_features = len(lag_values) + 1
+
     train_array = train_df.drop('Page', axis=1).values
 
     X_train = np.empty((n_series, n_days, n_features))
-    for i in range(len(train_array)):
-        if (i % 10000) == 0:
-            print(i)
+    for i in tqdm(range(len(train_array))):
         x_t = train_array[i]
-        features = get_lag_optimized(x_t, [365, 180, 90], n_days, n_features)
-
+        features = get_lag_optimized(x_t, lag_values, n_days)
         X_train[i, ] = features
 
     np.save('features_array.npy', X_train)
     return X_train
+
+
+n_features = 5
+X_train = create_features_optimized(DATA_PATH)
